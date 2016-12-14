f18681184a4f161fa3df7a6bb54cad4a09c1f1e8
R.A. Lucas <richard@richardalucas.com>
2016-12-13 23:13:05 -0700

    updated with monoalpha

--- monoalpha.js
+++ monoalpha.js
@@ -12,12 +12,15 @@ function invertMap(obj) {
   return output;
 }
 
-function getRandomLetter() {
-  var charA = 'A'.charCodeAt(0);
-  var charZ = 'Z'.charCodeAt(0);
+function getRandomIndex(arr) {
   return Math.floor(
-    Math.random() * (charZ - charA)
+    Math.random() * arr.length 
   );
+}
+
+function range(a, b) {
+  var len = Math.abs(b - a) + 1;
+  return Array.apply(null, {length: len}).map((n, i) => a + i); 
 }
 
 function Monoalphabetic() {
@@ -28,18 +31,31 @@ Monoalphabetic.prototype.createMap = function createMap() {
   var output = {};
   var charA = 'A'.charCodeAt(0);
   var charZ = 'Z'.charCodeAt(0);
-  var tmpArray = [];
+  var letterArr = range(charA, charZ);
   for (var i = 0; i < 26; i++) {
     var key = String.fromCharCode(charA + i);
-    var randomLetter = getRandomLetter();
-    if ( ~tmpArray.indexOf(randomLetter) ) {
-      output[key] = randomLetter; 
-      tmpArray.push(randomLetter);
-    } else {
-      // try to get another random letter 
+    var randomIdx = getRandomIndex(letterArr);
+    if (letterArr.length == 1) {
+      randomIdx = 0;
     }
+    output[key] = String.fromCharCode(letterArr[randomIdx]); 
+    letterArr.splice(randomIdx, 1);
   }
   return output; 
 };
 
+Monoalphabetic.prototype.encrypt = function encrypt(plainText) {
+  return plainText.split(' ').map(word => {
+      return word.split('').map(l => this.map[l]).join('');
+    }).join(' ');
+};
+
+Monoalphabetic.prototype.decrypt = function decrypt(cipherText) {
+  var invertedMap = invertMap(this.map);
+  return cipherText.split(' ').map(word => {
+      return word.split('').map(l => invertedMap[l]).join('');
+    }).join(' ');
+};
+
 module.exports = Monoalphabetic;
+
---
+++ simple_hash.js
@@ -0,0 +1,1 @@
+//XOR of every block
